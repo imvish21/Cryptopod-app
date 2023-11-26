b199b9c5a3940832e92f630aa38ff64fba2a942c
gauravpandey03 <gauravpandey10082003@gmail.com>
2023-11-26 10:39:41 +0530

    trade blockchain

--- src/Components/MainDashboard/Trade/Modal.jsx
+++ src/Components/MainDashboard/Trade/Modal.jsx
@@ -1,41 +1,41 @@
-// import './modal.css';
-// import Lottie from 'react-lottie';
-// import animationData from './transdone.json';
-// import "@fontsource/rubik";
-// import "@fontsource/poppins";
-// import '@fortawesome/fontawesome-free/css/all.css';
+import './modal.css';
+import Lottie from 'react-lottie';
+import animationData from './transdone.json';
+import "@fontsource/rubik";
+import "@fontsource/poppins";
+import '@fortawesome/fontawesome-free/css/all.css';
 
 
-// const Modal = ({modal,onClose}) => {
+const Modal = ({modal,onClose}) => {
 
-//   if(!modal){
-//     return null;
-//   }
-//   const defaultOptions = {
-//     loop: false,
-//     autoplay: true,
-//     animationData: animationData,
-//     rendererSettings: {
-//       preserveAspectRatio: "xMidYMid slice"
-//     }
-//   }
+  if(!modal){
+    return null;
+  }
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  }
 
-//   return (
-//     <div className='modalWrapper'>
-//       <div className="modal">
-//       <div className="btnClose">
-//         <i onClick={onClose} className="fa-regular fa-circle-xmark fa-lg"></i>
-//       </div>
-//         <Lottie 
-// 	    options={defaultOptions}
-//         height={200}
-//         width={250}
-//         className="lotties"
-//       />
-//         <h1 className='modalheading'>Transaction Done</h1>
-//       </div>
-//     </div>
-//   )
-// }
+  return (
+    <div className='modalWrapper'>
+      <div className="modal">
+      <div className="btnClose">
+        <i onClick={onClose} className="fa-regular fa-circle-xmark fa-lg"></i>
+      </div>
+        <Lottie 
+	    options={defaultOptions}
+        height={200}
+        width={250}
+        className="lotties"
+      />
+        <h1 className='modalheading'>Transaction Done</h1>
+      </div>
+    </div>
+  )
+}
 
-// export default Modal
+export default Modal
